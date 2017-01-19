22b40bfa885c2eb06bc1dc83dca94fa637ad3112
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2017-01-18 18:53:04 -0800

    Create Base for Shooter Vision
    Just a basic outline for the shooter outline class

--- src/org/usfirst/frc/team2635/robot/ShooterVision.java
+++ src/org/usfirst/frc/team2635/robot/ShooterVision.java
@@ -1,5 +1,17 @@
 package org.usfirst.frc.team2635.robot;
 
 public class ShooterVision {
-
+	
+	public double createBox(){
+		
+	}
+	
+	public double getDistance(){
+		
+	}
+	
+	public double getAngle(){
+		
+	}
+	
 }
