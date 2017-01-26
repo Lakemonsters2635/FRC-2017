4ec2127003e3a9b0d764edd32a748c5d1fce0914
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2017-01-25 20:46:39 -0800

    Working Create Box for ShooterVision
    Added working rectangles and bounding rectangles to shootervision. Does
    not check for best fit. (TB)

--- src/org/usfirst/frc/team2635/robot/ShooterVision.java
+++ src/org/usfirst/frc/team2635/robot/ShooterVision.java
@@ -1,7 +1,11 @@
 package org.usfirst.frc.team2635.robot;
 
+import java.util.ArrayList;
+
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import edu.wpi.cscore.CvSink;
@@ -14,23 +18,31 @@ public class ShooterVision {
 	Mat source;
 	GripPipeline GripPipeline;
 	CvSource cvSource;
-	MatOfPoint new_mat1;
+	ArrayList<Rect> boundRect;
 	
 	public void shooterCamInit(){
 		GripPipeline = new GripPipeline();
 		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
 		camera.setResolution(640, 480);
 		cvSink = new CvSink("cvSink");
+		cvSink.setSource(camera);
 		source = new Mat();
 		cvSource = CameraServer.getInstance().putVideo("new", 640, 480);
 	}
 	
 	public void createBox(){
+		boundRect = new ArrayList<Rect>();
 		cvSink.grabFrame(source);
 		GripPipeline.process(source);
-		new_mat1 = new MatOfPoint(source);
-		Imgproc.boundingRect(new_mat1);
-		cvSource.putFrame(new_mat1);
+		ArrayList<MatOfPoint> grip = GripPipeline.findContoursOutput();
+		for( int i = 0; i< grip.size(); i++ ){
+			Imgproc.drawContours(source, grip, i, new Scalar(0, 255,0),1);	
+			boundRect.add(Imgproc.boundingRect(grip.get(i)));
+			Rect rect = Imgproc.boundingRect(grip.get(i));
+			Imgproc.rectangle( source, rect.tl(), rect.br(), new Scalar(0,0,255), 2, 8, 0 );
+		    }
+		cvSource.putFrame(source);
+		
 		
 	}
 	
