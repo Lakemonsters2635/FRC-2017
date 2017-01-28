cefbc35fb6e975ccac6b2d0ab687a1c9133d0cfd
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2017-01-28 14:36:29 -0800

    Remove single unused variable

--- src/org/usfirst/frc/team2635/robot/Robot.java
+++ src/org/usfirst/frc/team2635/robot/Robot.java
@@ -111,8 +111,7 @@ public class Robot extends IterativeRobot {
     public void autonomousPeriodic() {
         Scheduler.getInstance().run();
     }
-int heyheyhey;
-    public void teleopInit() {
+public void teleopInit() {
 		// This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to 
         // continue until interrupted by another command, remove
